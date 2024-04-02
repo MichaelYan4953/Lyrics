9e433f11cf6fbf29002f4b9a343ce5fb1ccc89e9
MichaelYan4953 <157666487+MichaelYan4953@users.noreply.github.com>
2024-04-02 12:36:47 -0400

    Add files via upload

---
+++ Lyrics/Lyrics/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+class Lyrics
+{
+    static void Main()
+    {
+        WriteLine("Almost heaven, West Virginia");
+        WriteLine("Blue Ridge Mountains, Shenandoah River");
+        WriteLine("Life is old there, older than the trees");
+        WriteLine("Younger than the mountains, growin' like a breeze");
+    }
+}
